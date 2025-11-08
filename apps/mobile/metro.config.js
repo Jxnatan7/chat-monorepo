27ed35f487766111547ddedbc4140cdd4d17a8e7
Jonatan Santos <jonatan.santos.contato@gmail.com>
2025-11-08 16:33:01 -0300

    add mobile app

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../../");
+
+const config = getDefaultConfig(projectRoot);
+
+config.watchFolders = [workspaceRoot];
+
+config.resolver = {
+  ...config.resolver,
+  extraNodeModules: new Proxy(
+    {},
+    {
+      get: (_, name) => path.join(projectRoot, "node_modules", name),
+    }
+  ),
+};
+
+module.exports = config;
